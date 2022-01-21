a1b2806ecd90bad4620fb579f3993f989240ffc6
a head full of dreams <a head full of dreams>
2022-01-21 08:04:25 +0530

    If Else Or

--- helloScript.sh
+++ helloScript.sh
@@ -37,3 +37,29 @@ then
 else
 	echo "Age is not corrent"
 fi
+
+#if [[ "age" -lt 40 ]] || [[ "age" -gt 18 ]]			#Multiple ways to write the same if statements use either one
+#if [[ "age" -gt 40 || "age" -lt 18 ]]					#Multiple ways to write the same if statements use either one
+#then
+#	echo "Age is correct"
+#else
+#	echo "Age is not corrent"
+#fi
+
+#case $age in											#Switch Case for numbers
+#	18) 
+#		echo "18";;
+#	20)
+#		echo "20";;
+#	*) echo "Default";;
+#esac
+
+echo -n "Enter your country"							#Switch Case for Strings
+read country
+
+case $country in
+	Luthinia) echo  -n "Luthinia";;
+	Romania|Moldov) echo -n "Romanian";;
+	India) echo -n "Hindi";;
+	*) echo -n "Unknown";;
+esac
